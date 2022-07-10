d8837b88b97f3375a52216878cf642a9a4671c5a
JJ Uzumaki <104718343+xpunk7@users.noreply.github.com>
2022-07-10 19:40:53 +0200

    Create update-api.sh

---
+++ tools/update-api.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source "${__dir}/API_SHAS"
+
+protodir="${__dir}/../src/main/proto"
+tmpdir=`mktemp -d 2>/dev/null || mktemp -d -t 'tmpdir'`
+
+# Check if the temp dir was created.
+if [[ ! "${tmpdir}" || ! -d "${tmpdir}" ]]; then
+  echo "Could not create temp dir"
+  exit 1
+fi
+
+# Clean up the temp directory that we created.
+function cleanup {
+  rm -rf "${tmpdir}"
+}
+
+# Register the cleanup function to be called on the EXIT signal.
+trap cleanup EXIT
+
+pushd "${tmpdir}" >/dev/null
+
+rm -rf "${protodir}"
+
+echo "*.proto" > include-proto
+
+curl -sL https://github.com/envoyproxy/envoy/archive/${ENVOY_SHA}.tar.gz | tar xz --wildcards -T include-proto
+mkdir -p "${protodir}/envoy"
+cp -r envoy-*/api/envoy/* "${protodir}/envoy"
+
+curl -sL https://github.com/googleapis/googleapis/archive/${GOOGLEAPIS_SHA}.tar.gz | tar xz --wildcards -T include-proto
+mkdir -p "${protodir}/google/api"
+mkdir -p "${protodir}/google/api/expr/v1alpha1"
+mkdir -p "${protodir}/google/rpc"
+cp googleapis-*/google/api/annotations.proto googleapis-*/google/api/http.proto "${protodir}/google/api"
+cp googleapis-*/google/api/expr/v1alpha1/syntax.proto "${protodir}/google/api/expr/v1alpha1"
+cp googleapis-*/google/api/expr/v1alpha1/checked.proto "${protodir}/google/api/expr/v1alpha1"
+cp googleapis-*/google/rpc/status.proto "${protodir}/google/rpc"
+
+curl -sL https://github.com/lyft/protoc-gen-validate/archive/${PGV_GIT_SHA}.tar.gz | tar xz --wildcards -T include-proto
+mkdir -p "${protodir}/validate"
+cp -r protoc-gen-validate-*/validate/* "${protodir}/validate"
+
+curl -sL https://github.com/census-instrumentation/opencensus-proto/archive/${OPENCENSUS_SHA}.tar.gz | tar xz --wildcards -T include-proto
+mkdir -p "${protodir}/opencensus/proto"
+cp -r opencensus-proto-*/src/opencensus/proto/* "${protodir}/opencensus/proto"
+
+curl -sL https://github.com/prometheus/client_model/archive/${PROMETHEUS_SHA}.tar.gz | tar xz --wildcards -T include-proto
+cp client_model-*/metrics.proto "${protodir}"
+
+curl -sL https://github.com/cncf/udpa/archive/${UDPA_SHA}.tar.gz | tar xz --wildcards -T include-proto
+mkdir -p "${protodir}/udpa"
+mkdir -p "${protodir}/xds"
+cp -r udpa-*/udpa/* "${protodir}/udpa"
+cp -r udpa-*/xds/* "${protodir}/xds"
+
+popd >/dev/null
